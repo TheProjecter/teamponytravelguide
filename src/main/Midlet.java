652e00da4c41b460240c52711eb02e06c6262cb5
teamponySE3 <teamponySE3@1670192a-1405-11df-b09a-cf1a22568e95>
2010-02-16 14:18:17 +0000

    updated

---
+++ src/main/Midlet.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author nialldeasy
+ */
+public class Midlet extends MIDlet {
+    public void startApp() {
+    }
+
+    public void pauseApp() {
+    }
+
+    public void destroyApp(boolean unconditional) {
+    }
+}
